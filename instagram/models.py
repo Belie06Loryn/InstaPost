5199ea909438f863b75fb3eaf03c2fd0cd476df1
Belie06Loryn <maniralexie@gmail.com>
2019-10-17 10:35:31 +0200

    insirting image class with save,delete,update method

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Foto(models.Model):
+    image = models.ImageField(upload_to = 'photos/',null=True)
+    name = models.CharField(max_length =40)
+    caption = models.TextField(max_length =6000)
+    like = models.TextField(max_length =6000)
+    profile = models.ForeignKey(Profile)
+    comments = models.ForeignKey(Comment ,null=True)
+     
+    
+    def save_pic(self):
+        self.save()
+
+    def dele_pic(self):
+        self.delete() 
+
+    @classmethod
+    def image_by_id(cls,id):
+        found = cls.objects.filter(id = id)
+        return found
+
+    @classmethod
+    def update_pic(cls,id):
+        imaje = cls.objects.filter(id=id).update(id=id)
+        return imaje    
