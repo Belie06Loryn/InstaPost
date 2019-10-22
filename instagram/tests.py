2277fb87b3025957448663a760cb7b8009045451
Belie06Loryn <maniralexie@gmail.com>
2019-10-22 20:36:09 +0200

    testing profile class

--- instagram/tests.py
+++ instagram/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Foto,Comment,Follower,Profile
+class ProfileTestClass(TestCase):
 
-# Create your tests here.
+    # Set up method
+    def setUp(self):
+        self.profile= Profile(image = 'Jam.jpeg', name ='Muriuki', email ='james@moringaschool.com',bio = 'hdeydfedf')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(self.profile,Follower)
+
+    def tearDown(self):
+        Follower.objects.all().delete()
+   
+    
+               
+        
+ 
